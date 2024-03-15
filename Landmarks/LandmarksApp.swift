0585a43964181719c490fa524f9be44cdb9e6e95
Sasha Jaroshevskii <jaroshevskiisasha@gmail.com>
2024-03-15 02:54:25 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sasha Jaroshevskii on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

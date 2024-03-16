f1d824ffd58d330b0237de28c103e695f2a62d59
Sasha Jaroshevskii <jaroshevskiisasha@gmail.com>
2024-03-16 19:23:44 +0200

    XXXX

--- Landmarks/CircleImage.swift
+++ Landmarks/CircleImage.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(.turtlerock)
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
     }
 }
 

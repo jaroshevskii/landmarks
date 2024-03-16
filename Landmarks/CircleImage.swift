6b05a65f2956a7d3203cf40c375371fb6182cb1b
Sasha Jaroshevskii <jaroshevskiisasha@gmail.com>
2024-03-16 19:17:28 +0200

    Section 4 Create a custom image view

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Sasha Jaroshevskii on 16.03.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
